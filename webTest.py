56ae13abf25e59af64c5c406a36d8a76b1f12cba
Ethan Young <ethanyoung@hotmail.co.uk>
2022-04-19 21:17:54 +0100

    added scrolling and load more products functionality to navigate page

--- webTest.py
+++ webTest.py
@@ -1,5 +1,9 @@
+from json import load
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 import time
 
 #//*[@id="onetrust-accept-btn-handler"]
@@ -11,16 +15,24 @@ class Web_Scraper:
         self.driver.get("https://uk.gymshark.com/collections/all-products/mens")
         
     def accept_Cookies(self):
-        time.sleep(3)
+        delay = 10
         try:
-            accept_cookies_button = self.driver.find_element_by_xpath('//*[@id="onetrust-accept-btn-handler"]')
+            WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')))
+            accept_cookies_button = WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')))
             accept_cookies_button.click()
-        except:
-            pass # If there is no cookies button, we won't find it, so we can pass
+            time.sleep(1)
+        except TimeoutException:
+            print("Loading took too much time!")
 
     def navigate_WebPage(self):
-        element = self.driver.find_element_by_xpath("/html/body/div[2]/div[2]/main/div[2]/div[1]/section/button")
-        element.click()
+        while True:
+            if not self.driver.find_element_by_xpath('//*[@class="Styles__Button-sc-1kpnvfh-4 cRnfyG Styles__PaginationButton-sc-1kf2zc1-1 hBnwhL"]').is_displayed():
+                break
+            else:
+                time.sleep(4)
+                self.driver.execute_script("window.scrollTo(0, (document.body.scrollHeight) - 1200);")
+                load_More_Button = self.driver.find_element_by_xpath('//*[@class="Styles__Button-sc-1kpnvfh-4 cRnfyG Styles__PaginationButton-sc-1kf2zc1-1 hBnwhL"]')
+                load_More_Button.click()
 
     def get_Links(self):
         time.sleep(3)
@@ -39,5 +51,5 @@ class Web_Scraper:
 if __name__ == '__main__':
     test = Web_Scraper()
     test.accept_Cookies()
-    #test.navigate_WebPage()
-    test.get_Links()
+    test.navigate_WebPage()
+    #test.get_Links()
