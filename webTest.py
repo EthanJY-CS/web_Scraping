8cf6f4babc99774f99ddd88d9d3a535575e6316f
Ethan Young <ethanyoung@hotmail.co.uk>
2022-04-21 15:42:48 +0100

    Added functionality to click on different product types ready for web crawler to use

--- webTest.py
+++ webTest.py
@@ -6,13 +6,11 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 import time
 
-#//*[@id="onetrust-accept-btn-handler"]
-
 class Web_Scraper:
 
-    def __init__(self):
+    def __init__(self, url):
         self.driver = webdriver.Firefox()
-        self.driver.get("https://uk.gymshark.com/collections/all-products/mens")
+        self.driver.get(url)
         
     def accept_Cookies(self):
         delay = 10
@@ -29,12 +27,26 @@ class Web_Scraper:
             if not self.driver.find_element_by_xpath('//*[@class="Styles__Button-sc-1kpnvfh-4 cRnfyG Styles__PaginationButton-sc-1kf2zc1-1 hBnwhL"]').is_displayed():
                 break
             else:
-                time.sleep(4)
+                time.sleep(2)
                 self.driver.execute_script("window.scrollTo(0, (document.body.scrollHeight) - 1200);")
                 load_More_Button = self.driver.find_element_by_xpath('//*[@class="Styles__Button-sc-1kpnvfh-4 cRnfyG Styles__PaginationButton-sc-1kf2zc1-1 hBnwhL"]')
                 load_More_Button.click()
 
-    def get_Links(self):
+    def get_Product_Catagories(self):
+        time.sleep(3)
+        catagory_container = self.driver.find_element_by_xpath('//*[@id="collections"]') # XPath corresponding to the Container
+        product_Type_Button = catagory_container.find_element_by_xpath('./button')
+        product_Type_Button.click()
+
+        checkbox_list = catagory_container.find_elements_by_xpath('//*[@class="Styles__Checkbox-sc-9kless-1 eLKrVj"]')
+        print(f'There are {len(checkbox_list)} catagories on this page')
+        for checkbox in checkbox_list:
+            time.sleep(4)
+            checkbox.click()
+            time.sleep(2)
+            checkbox.click()
+
+    def get_Product_Links(self):
         time.sleep(3)
         prod_container = self.driver.find_element_by_xpath('//*[@class="Styles__Grid-sc-1hr3n2q-0 ekxOoE"]') # XPath corresponding to the Container
         prod_list = prod_container.find_elements_by_xpath('./article')
@@ -49,7 +61,13 @@ class Web_Scraper:
         print(link_list)
 
 if __name__ == '__main__':
-    test = Web_Scraper()
+    mens_URL = "https://uk.gymshark.com/collections/all-products/mens"
+    womens_URL = "https://uk.gymshark.com/collections/all-products/womens"
+    test = Web_Scraper(mens_URL)
     test.accept_Cookies()
-    test.navigate_WebPage()
-    #test.get_Links()
+    test.get_Product_Catagories()
+    #test.navigate_WebPage()
+    #test.get_Product_Links()
+
+
+#Styles__Wrapper-sc-9kless-0 jZLLBP
