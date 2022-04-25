4f7a3f266479c691f165c515c8217308018b2725
Ethan Young <ethanyoung@hotmail.co.uk>
2022-04-25 22:07:57 +0100

    Added functionality of scraping the individual pages and creating data dictionaries for each product

--- webTest.py
+++ webTest.py
@@ -1,8 +1,5 @@
-from cgi import test
-from json import load
-from xml.dom import NotFoundErr
+from math import prod
 from selenium import webdriver
-import selenium
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -22,20 +19,16 @@ class Web_Scraper:
             WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')))
             accept_cookies_button = WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')))
             accept_cookies_button.click()
-            time.sleep(1)
         except TimeoutException:
             print("Loading took too much time!")
 
     def load_More_Products(self):
-        delay = 20
-        while True:
+        for i in range(1):
             try:
-                WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@class="Styles__Button-sc-1kpnvfh-4 cRnfyG Styles__PaginationButton-sc-1kf2zc1-1 hBnwhL"]')))
                 load_More_Button = self.driver.find_element_by_xpath('//*[@class="Styles__Button-sc-1kpnvfh-4 cRnfyG Styles__PaginationButton-sc-1kf2zc1-1 hBnwhL"]')
             except:
                 break
             else:
-                time.sleep(2)
                 self.driver.execute_script("window.scrollTo(0, (document.body.scrollHeight) - 1200);")
                 load_More_Button.click()
         self.driver.execute_script("window.scrollTo(0, 0);")
@@ -63,20 +56,58 @@ class Web_Scraper:
             link = a_tag.get_attribute('href')
             link_list.append(link)
         print(f'There are {len(link_list)} Products on this page')
-        print(link_list)
+        #print(link_list)
         return link_list
 
-    def generate_ID(self, link_list):
-        id_list = []
-        uuid_list = []
-        for link in link_list:
-            idx = link.find('gymshark-', 0, len(link))
-            id_list.append(link[idx:len(link)])
-            uuid_list.append(str(uuid.uuid4()))
-        return id_list, uuid_list
+    def generate_ID(self, link):
+        idx = link.find('gymshark-', 0, len(link))
+        id = link[idx:len(link)]
+        uu_ID = str(uuid.uuid4())
+        return id, uu_ID
     
-    def scrape_Data(self):
-        pass
+    def scrape_Data(self, link_HTML):
+        delay = 10
+        try:
+            WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@class="Styles__LeftColumn-lpqwbz-2 danMny"]')))
+        except TimeoutException:
+            print("Loading took too much time!")
+        
+        images_container = self.driver.find_element_by_xpath('//*[@class="Styles__LeftColumn-lpqwbz-2 danMny"]')
+        details_container = self.driver.find_element_by_xpath('//*[@class="Styles__RightColumn-lpqwbz-3 beeZhk"]')
+
+        #Scrape Images
+        image_list = images_container.find_elements_by_xpath('.//*[@class="Styles__Image-sc-176u4ag-1 etChzz"]')
+        image_links = []
+        for image in image_list:
+            img_tag = image.find_element_by_tag_name('img')
+            link = img_tag.get_attribute('src')
+            image_links.append(link)
+    
+        #Scrape Details
+        product_title = details_container.find_element_by_xpath('.//h1').text
+        product_gender = details_container.find_element_by_xpath('.//h5').text
+        product_price = details_container.find_element_by_xpath('.//span[@class="Styles__Price-qfm034-4 laqfaf"]').text
+        product_sizes_container = details_container.find_element_by_xpath('.//div[@class="Styles__SizesWrapper-sc-8ocgtc-2 kAvucC"]')
+        product_sizes = product_sizes_container.find_elements_by_xpath('./button')
+        sizes_list = []
+        for size in product_sizes:
+            sizes_list.append(size.get_attribute('aria-label'))
+        id, uu_ID = self.generate_ID(link_HTML)
+
+        #Create dictionary from the data collected
+        product_Dict = {
+            "UUID": uu_ID,
+            "ID": id,
+            "Title": product_title,
+            "Catalogue": product_gender,
+            "Price": product_price,
+            "Sizes": sizes_list,
+            "Images": image_links
+        }
+
+        print("\n")
+        print(product_Dict)
+        print("\n")
 
     def load_Product_Links(self, link_list):
         window_before = self.driver.window_handles[0]
@@ -85,24 +116,21 @@ class Web_Scraper:
             window_after = self.driver.window_handles[1]
             self.driver.switch_to.window(window_after)
             self.driver.get(link)
+            self.scrape_Data(link)
             self.driver.close()
-            time.sleep(3)
-            break
+            break #Take this out When we want to scrape all the links not just 1 per product type ##Testing purposes!
         self.driver.switch_to.window(window_before)
 
     def start_crawl(self):
         self.accept_Cookies()
         product_Type_Button, checkbox_list = self.get_product_types()
         for checkbox in checkbox_list:
-            time.sleep(2)
             checkbox.click()
             product_Type_Button.click()
             self.load_More_Products()
             link_list = self.get_Product_Links()
-            id_list, uuid_list = self.generate_ID(link_list)
             self.load_Product_Links(link_list)
             product_Type_Button.click()
-            time.sleep(2)
             checkbox.click()
 
 if __name__ == '__main__':
