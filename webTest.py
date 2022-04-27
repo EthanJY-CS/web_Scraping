e6a8de20198c5bc569937337bc59c2446f2f01f8
Ethan Young <ethanyoung@hotmail.co.uk>
2022-04-27 23:10:56 +0100

    Refactored and Added DocString to document

--- webTest.py
+++ webTest.py
@@ -1,7 +1,6 @@
-from math import prod
-from tabnanny import check
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
@@ -11,26 +10,96 @@ import json
 import requests
 
 class Web_Scraper:
+    '''
+    A Web Scraper that is as generic as possible when regarding to control flow and processes 
+    but tailored for the gymshark website, collecting the Mens catalogue and the Womens.
+    
+    Parameters:
+    ----------
+    url: str
+        str of the HTML link of the website to provide
+    catalogue: str
+        str of the catalogue, either 'Mens' or 'Womens'
+    
+    Attributes:
+    ----------
+    options: WebDriverOptions
+        WebDriverOptions to be used when wanting to run in headless mode
+    driver: WebDriver
+        Webdriver to be used to drive the website UI, from Selenium
+    root_Path: str
+        str of the root_path to the environments directory
+    current_Directory: str
+        str of the path of current directory that will be concatenated with root_Path
+    catalogue: str
+        str of the catalogue, used for directory creation, passed from parameter
+     
+    Methods:
+    -------
+    create_Directory()
+        Creates a Directory
+    download_Image(image_url, image_name)
+        Downloads the image of the content at the url link provided
+    accept_Cookies()
+        Accepts the cookies button upon load of webpage
+    load_More_Products()
+        Navigates to bottom of page and clicks load more button until all products show
+    get_Product_Types()
+        Gets the product types from the filter bar and their checkboxes to navigate
+    get_Product_Links()
+        Gets a List of all the HTML links from the products
+    generate_ID(link)
+        Creates the ID and UUID of a product
+    scrape_Data(link_HTML)
+        Collects all data from a products webpage
+    load_Product_Links(link_list)
+        Loads each products webpage in new tab ready to scrape
+    start_Crawl()
+        Main control of flow function to call all other functions from in order
+    '''
 
     def __init__(self, url, catalogue):
-        self.driver = webdriver.Firefox()
+        self.options = Options()
+        self.options.headless = False #Change to True when scraping Data
+        self.driver = webdriver.Firefox(options=self.options)
         self.driver.get(url)
         self.root_Path = "/home/ethanjy/Scratch/web_Scraping/raw_data"
-        self.current_directory = ""
+        self.current_Directory = ""
         self.catalogue = catalogue
         
-    def create_folder(self):
-        Path(self.root_Path + self.current_directory).mkdir(parents=True, exist_ok=True)
+    def create_Directory(self):
+        '''
+        Creates a Directory.
+        The Directory is created at the concatenation of the root_Path and current_Directory
+        '''
+        Path(self.root_Path + self.current_Directory).mkdir(parents=True, exist_ok=True)
     
     def download_Image(self, image_url, image_name):
+        '''
+        Downloads the image of the content at the url link provided.
+        The Images are downloaded in an images directory, named 'n.jpg'
+        where n is an integer starting at 0 to n images
+
+        Parameters:
+        ----------
+        image_url: str
+            The url link of the img source scraped
+        image_name: str
+            The integer idx of the image passed as string
+        '''
         img_data = requests.get(image_url).content
-        self.current_directory += "/images"
-        self.create_folder() #Creates folder for images inside the
-        with open(self.root_Path + self.current_directory + "/" + image_name + '.jpg', 'wb') as handler:
+        self.current_Directory += "/images"
+        self.create_Directory() #Creates Directory for images inside the
+        with open(self.root_Path + self.current_Directory + "/" + image_name + '.jpg', 'wb') as handler:
             handler.write(img_data)
-        self.current_directory = self.current_directory.replace("/images", "")
+        self.current_Directory = self.current_Directory.replace("/images", "")
 
     def accept_Cookies(self):
+        '''
+        Accepts the cookies button upon load of webpage.
+        The driver waits for the container of the accept cookies handler,
+        once it loads, presses the accept cookies button.
+        '''
         delay = 10
         try:
             WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')))
@@ -40,6 +109,11 @@ class Web_Scraper:
             print("Loading took too much time!")
 
     def load_More_Products(self):
+        '''
+        Navigates to bottom of page and clicks load more button until all products show
+        While there are more products to load, navigate to bottom of page and click
+        load more button, all products from a product type will load on the same page.
+        '''
         while True:
             try:
                 load_More_Button = self.driver.find_element_by_xpath('//*[@class="Styles__Button-sc-1kpnvfh-4 cRnfyG Styles__PaginationButton-sc-1kf2zc1-1 hBnwhL"]')
@@ -49,7 +123,19 @@ class Web_Scraper:
                 break 
         self.driver.execute_script("window.scrollTo(0, 0);")
 
-    def get_product_types(self):
+    def get_Product_Types(self):
+        '''
+        Gets the product types from the filter bar and their checkboxes to navigate.
+        Waits for the page to load filter bar, clicks on product_Type filter, and grabs
+        all the product_Type checkboxes.
+
+        Returns:
+        -------
+        product_Type_Button: WebElement
+             the product_Type button to click to reveal from the filter bar
+        checkbox_list: list[WebElement]
+            list of WebElement checkboxes that correspond to each of the product_Types filters
+        '''
         delay = 10
         try:
             WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="collections"]')))
@@ -63,6 +149,17 @@ class Web_Scraper:
         return product_Type_Button, checkbox_list
 
     def get_Product_Links(self):
+        '''
+        Gets a List of all the HTML links from the products.
+        Finds container that holds all the products, then finds all /article
+        from it's children, as these are all the individual products.
+        For each product, find the HTML href link tag and append it to the list.
+
+        Returns:
+        -------
+        link_list: List[str]
+             list of all the HTML links to all products loaded on a page
+        '''
         prod_container = self.driver.find_element_by_xpath('//*[@class="Styles__Grid-sc-1hr3n2q-0 ekxOoE"]')
         prod_list = prod_container.find_elements_by_xpath('./article')
         link_list = []
@@ -73,12 +170,42 @@ class Web_Scraper:
         return link_list
 
     def generate_ID(self, link):
+        '''
+        Creates the ID and UUID of a product.
+        The id is stripped from the HTML link, as it contains a unique user friendly ID.
+        The UUID is generated using UUID4 from the UUID library and stored as a string.
+
+        Parameters:
+        ----------
+        link: str
+            The HTML url link of the product
+
+        Returns:
+        -------
+        id: str
+            The id of the product
+        uu_ID: str
+            The generated UUID of the product
+        '''
         idx = link.find('gymshark-', 0, len(link))
         id = link[idx:len(link)]
         uu_ID = str(uuid.uuid4())
         return id, uu_ID
     
     def scrape_Data(self, link_HTML):
+        '''
+        Collects all data from a products webpage.
+        Waits for the left container to load on the webpage (chosen as its images so makes sense for loading time)
+        The data is split through left colunn containing the images and the right column containing the product
+        details. Scrapes all products details, grabs ID's from generate_ID() func, creates appropriate directories,
+        scrapes images and downloads them by the download_Image() func into an images directory, creates data dictionary 
+        of all scraped data, then creates a json file of the data dictionary.
+        
+        Parameters:
+        ----------
+        link_HTML: str
+            The HTML url link of the product
+        '''
         delay = 10
         try:
             WebDriverWait(self.driver, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@class="Styles__LeftColumn-lpqwbz-2 danMny"]')))
@@ -100,10 +227,10 @@ class Web_Scraper:
                 sizes_list.append(size.get_attribute('aria-label'))
             id, uu_ID = self.generate_ID(link_HTML)
 
-            self.current_directory += "/" + product_title
-            self.create_folder() #Creates a directory of that product title! As they come in different colours, this is where they are stored under a single folder
-            self.current_directory += "/" + product_colour
-            self.create_folder() #Creates a directory of the specific product by id which will containt it's data json file and images directory
+            self.current_Directory += "/" + product_title
+            self.create_Directory() #Creates a directory of that product title! As they come in different colours, this is where they are stored under a single directory
+            self.current_Directory += "/" + product_colour
+            self.create_Directory() #Creates a directory of the specific product by id which will containt it's data json file and images directory
 
             #Scrape Images
             image_list = images_container.find_elements_by_xpath('.//*[@class="Styles__Image-sc-176u4ag-1 etChzz"]')
@@ -129,14 +256,25 @@ class Web_Scraper:
                 "Images": image_links
             }
 
-            with open(self.root_Path + self.current_directory + '/data.json', 'w') as fp:
+            with open(self.root_Path + self.current_Directory + '/data.json', 'w') as fp:
                 json.dump(product_Dict, fp)
-            self.current_directory = self.current_directory.replace("/" + product_colour, "")
-            self.current_directory = self.current_directory.replace("/" + product_title, "")
+            self.current_Directory = self.current_Directory.replace("/" + product_colour, "")
+            self.current_Directory = self.current_Directory.replace("/" + product_title, "")
         except TimeoutException:
             print("Loading took too much time!")
 
     def load_Product_Links(self, link_list):
+        '''
+        Loads each products webpage in new tab ready to scrape.
+        Opens a new tab, switches the window to new tab, then loads all products url from link_list,
+        one by one, which we then call scrape_Data() func to carry out the scraping. After all
+        products have been scraped, the tab closes, and window switches back to main window.
+
+        Parameters:
+        ----------
+        link_list: list[str]
+            The list of HTML links of all the products
+        '''
         window_before = self.driver.window_handles[0]
         self.driver.execute_script("window.open()")
         for link in link_list:
@@ -144,29 +282,37 @@ class Web_Scraper:
             self.driver.switch_to.window(window_after)
             self.driver.get(link)
             self.scrape_Data(link)
-            break #Take this out When we want to scrape all the links not just 1 per product type ##Testing purposes!
+            #break #Take this out When we want to scrape all the links not just 1 per product type ##Testing purposes!
         self.driver.close()
         self.driver.switch_to.window(window_before)
 
-    def start_crawl(self):
-        self.create_folder() #Creates raw_Data Directory
-        self.current_directory = "/" + self.catalogue
-        self.create_folder() #Creates catalogue Directory
+    def start_Crawl(self):
+        '''
+        Main control of flow function to call all other functions from in order.
+        Main function that controls the rest of the scraper class. Creates raw_Data Directory in root_Path,
+        then creates the catalogue it's scraping from. Accepts cookies then navigates the webpage by clicking
+        each product type checkbox in each loop from the filter bar, and then calling all necessary functions 
+        to perform the scraping in order. The driver then quits after performing.
+
+        '''
+        self.create_Directory() #Creates raw_Data Directory
+        self.current_Directory = "/" + self.catalogue
+        self.create_Directory() #Creates catalogue Directory
         self.accept_Cookies()
-        product_Type_Button, checkbox_list = self.get_product_types()
+        product_Type_Button, checkbox_list = self.get_Product_Types()
         for checkbox in checkbox_list:
             product_Type = checkbox.get_attribute('id').replace(" & ", " and ")
             if product_Type != 'Accessories': #In case of womens catalogue, they added accessories as a clothing type for some reason, so we ignore this
-                self.current_directory += "/" + product_Type
-                self.create_folder() #Creates product_Type directory
+                self.current_Directory += "/" + product_Type
+                self.create_Directory() #Creates product_Type directory
                 checkbox.click()
                 product_Type_Button.click()
-                #self.load_More_Products() #Take this out when we just want max 60 products
+                self.load_More_Products() #Take this out when we just want max 60 products
                 link_list = self.get_Product_Links()
                 self.load_Product_Links(link_list)
                 product_Type_Button.click()
                 checkbox.click()
-                self.current_directory = self.current_directory.replace("/" + product_Type, "")
+                self.current_Directory = self.current_Directory.replace("/" + product_Type, "")
                 #break #Take this out When we want to scrape all the links not just 1 per product type ##Testing purposes!
         self.driver.quit()
 
@@ -174,6 +320,6 @@ if __name__ == '__main__':
     mens_URL = "https://uk.gymshark.com/collections/all-products/mens"
     womens_URL = "https://uk.gymshark.com/collections/all-products/womens"
     mens_Catalogue = Web_Scraper(mens_URL, "Mens")
-    mens_Catalogue.start_crawl()
-    #womens_Catalogue = Web_Scraper(womens_URL, "Womens")
-    #womens_Catalogue.start_crawl()
+    mens_Catalogue.start_Crawl()
+    womens_Catalogue = Web_Scraper(womens_URL, "Womens")
+    womens_Catalogue.start_Crawl()
