4eae05d47ea5e9db9e6ef7575321b9807fd5d48c
Ethan Young <ethanyoung@hotmail.co.uk>
2022-05-04 17:06:42 +0100

    Added creation, adding and querying of rds database

--- cloud_data.py
+++ cloud_data.py
@@ -1,4 +1,8 @@
 import boto3
+import os
+from psycopg2 import connect
+from sqlalchemy import create_engine
+import pandas as pd
 
 def upload_to_s3(fileName, bucketName, objectName):
     s3_client = boto3.client('s3')
@@ -16,3 +20,52 @@ def download_files(bucketName, objectName, fileName):
     # Of course, change the names of the files to match your own.
     s3.download_file(bucketName, objectName, fileName)
 
+def upload_directory_to_s3():
+    s3_client = boto3.client('s3')
+    try:
+        root_path = "/home/ethanjy/Scratch/web_Scraping/raw_data" # local folder for upload
+        for path, subdirs, files in os.walk(root_path):
+            path = path.replace("\\","/")
+            directory_name = path.replace(root_path,"")
+            for file in files:
+                s3_client.upload_file(os.path.join(path, file), "gymshark-data", directory_name+'/'+file)
+    except Exception as err:
+        print(err)
+
+def create_RDS_engine():
+    DATABASE_TYPE = 'postgresql'
+    DBAPI = 'psycopg2'
+    ENDPOINT = 'gymshark-database.cealtvnlvdai.eu-west-1.rds.amazonaws.com' # Change it for your AWS endpoint
+    USER = 'postgres'
+    PASSWORD = 'EthanJY69' 
+    PORT = 5432
+    DATABASE = 'postgres'
+    engine = create_engine(f"{DATABASE_TYPE}+{DBAPI}://{USER}:{PASSWORD}@{ENDPOINT}:{PORT}/{DATABASE}")
+    return engine
+
+def add_record_to_rds(dict_ToAdd):
+    engine = create_RDS_engine()
+    connection = engine.connect()
+    record = pd.DataFrame([dict_ToAdd])
+    record.to_sql("gymshark-database", connection, if_exists='append')
+    connection.close()
+    engine.dispose()
+
+def read_database():
+    engine = create_RDS_engine()
+    connection = engine.connect()
+    df = pd.read_sql_table('gymshark-database', connection)
+    print(df.head())
+    connection.close()
+    engine.dispose()
+
+def does_record_exist(id):
+    engine = create_RDS_engine()
+    connection = engine.connect()
+    record = connection.execute("SELECT 1 FROM gymshark-database WHERE ID=%s;", id)
+    connection.close()
+    engine.dispose()
+    if record.rowcount() == 0:
+        return False
+    else:
+        return True
